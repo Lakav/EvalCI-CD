32a9643d5f8ea11add1ddb2a384bf3cbcb6d8914
julescavanier <cavanierju@gmail.com>
2025-07-03 13:48:17 +0200

    error message

--- frontend/src/user-register/user-register.component.ts
+++ frontend/src/user-register/user-register.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router, RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
@@ -18,23 +18,48 @@ export class UserRegisterComponent {
   errorMessage = '';
   successMessage = '';
 
-  constructor(private http: HttpClient, private router: Router) {}
+  constructor(private http: HttpClient, private router: Router, private cdr: ChangeDetectorRef) {}
 
   onSubmit() {
+    // Réinitialiser les messages
     this.errorMessage = '';
     this.successMessage = '';
+    
+    // Validation des champs
+    if (!this.nom || !this.prenom || !this.email || !this.password) {
+      this.errorMessage = 'Veuillez remplir tous les champs';
+      this.cdr.detectChanges();
+      return;
+    }
+    
+    // Appel à l'API d'inscription
     this.http.post<any>('http://localhost:3002/api/users/register', {
       nom: this.nom,
       prenom: this.prenom,
       email: this.email,
       password: this.password
     }).subscribe({
-      next: () => {
+      next: (res: any) => {
+        // Inscription réussie
         this.successMessage = 'Inscription réussie ! Vous pouvez vous connecter.';
+        this.cdr.detectChanges();
         setTimeout(() => this.router.navigate(['/user']), 1500);
       },
-      error: (err) => {
-        this.errorMessage = err.error?.message || 'Erreur lors de l\'inscription';
+      error: (err: any) => {
+        // Définir un message d'erreur par défaut
+        this.errorMessage = 'Erreur lors de l\'inscription';
+        
+        // Messages d'erreur spécifiques
+        if (err.status === 400) {
+          if (err.error?.message?.includes('email est déjà utilisé')) {
+            this.errorMessage = 'Cet email est déjà utilisé. Veuillez en choisir un autre.';
+          } else {
+            this.errorMessage = 'Tous les champs sont requis';
+          }
+        }
+        
+        // Forcer la détection de changements
+        this.cdr.detectChanges();
       }
     });
   }
