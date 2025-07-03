ed3307dabe3c7912cbf1c6b87701b17dda995cc5
julescavanier <cavanierju@gmail.com>
2025-07-03 11:37:34 +0200

    route fix

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,8 +4,8 @@ import { UserComponent } from '../app/user/user.component';
 import { UserRegisterComponent } from '../user-register/user-register.component';
 
 export const routes: Routes = [{
-  path:'todos',component:TodoComponent,pathMatch:'full'
-}, { path: '', redirectTo: 'todos', pathMatch: 'full' },
+  path:'',component:TodoComponent,pathMatch:'full'
+},
 { path: 'user', component:UserComponent, pathMatch: 'full' },
 { path: 'register', component: UserRegisterComponent, pathMatch: 'full' }
 ];
